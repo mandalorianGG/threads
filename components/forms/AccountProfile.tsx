19909b143c2cc907a4d7006eceafa8e68b434be1
mandalorianGG <skocny.jiri@gmail.com>
2023-08-28 07:35:39 +0200

    feat: handle image upload

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -18,6 +18,8 @@ import * as z from 'zod'
 import Image from 'next/image'
 import { ChangeEvent, useState } from 'react'
 import { Textarea } from '../ui/textarea'
+import { useUploadThing } from '@/lib/uploadthing'
+import { isBase64Image } from '@/lib/utils'
 
 interface Props {
   user: {
@@ -33,6 +35,7 @@ interface Props {
 
 const AccountProfile = ({ user, btnTitle }: Props) => {
   const [files, setFiles] = useState<File[]>([])
+  const { startUpload } = useUploadThing('media')
 
   const form = useForm({
     resolver: zodResolver(UserValidation),
@@ -69,8 +72,18 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
     }
   }
 
-  function onSubmit(values: z.infer<typeof UserValidation>) {
-    console.log(values)
+  const onSubmit = async (values: z.infer<typeof UserValidation>) => {
+    const blob = values.profile_photo
+
+    const hasImageChanged = isBase64Image(blob)
+
+    if (hasImageChanged) {
+      const imgRes = await startUpload(files)
+
+      if (imgRes && imgRes[0].fileUrl) {
+        values.profile_photo = imgRes[0].fileUrl
+      }
+    }
   }
 
   return (
